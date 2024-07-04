cb648cdcaf0db2bfa0e5427c624919211b58521a
Alexander <oops.krit@yandex.ru>
2024-07-04 22:52:03 +0300

    feat: add button navigation

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,25 @@
-import { registerPartials, buildTemplates, pageConfig } from './helpers'
+import { registerPartials, buildTemplates } from './helpers'
+import { Routes } from './app/types'
+import { appContainer, pageConfig } from './app/constants'
 import './style.scss'
-
-const appContainer = document.querySelector<HTMLDivElement>(
-    '#app',
-) as HTMLDivElement
 
 registerPartials()
 buildTemplates()
 
-function switchPage(newPage: string) {
+function switchPage(newPage: `${Routes}`) {
     appContainer.innerHTML = pageConfig[newPage]
 }
 
-document.addEventListener('DOMContentLoaded', () => switchPage('login'))
+document.addEventListener('DOMContentLoaded', () => {
+    switchPage(Routes.login)
+})
+
+document.addEventListener('click', (e) => {
+    e.preventDefault()
+    const to = (e.target as HTMLButtonElement).getAttribute('to')
+    const href = (e.target as HTMLLinkElement).getAttribute('href')
+
+    if (to || href) {
+        switchPage((to || href) as `${Routes}`)
+    }
+})
