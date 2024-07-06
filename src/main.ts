c5806d23121237ef5385f64db868ec4d32f4e6b2
Alexander <oops.krit@yandex.ru>
2024-07-06 12:44:43 +0300

    feat: add modal to profile-page; add toggle modal action

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,25 @@ import './style.scss'
 registerPartials()
 buildTemplates()
 
+let currentPage: `${Routes}`
+
 function switchPage(newPage: `${Routes}`) {
     appContainer.innerHTML = pageConfig[newPage]
+    currentPage = newPage
+}
+
+function showModal() {
+    const modalOverlay = document.getElementById(
+        'modal-overlay',
+    ) as HTMLDivElement
+    modalOverlay.classList.add('visible')
+}
+
+function closeModal() {
+    const modalOverlay = document.getElementById(
+        'modal-overlay',
+    ) as HTMLDivElement
+    modalOverlay.classList.remove('visible')
 }
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -21,5 +38,19 @@ document.addEventListener('click', (e) => {
 
     if (to || href) {
         switchPage((to || href) as `${Routes}`)
+    }
+
+    if (
+        currentPage === Routes.profile ||
+        currentPage === Routes.editProfile ||
+        currentPage === Routes.editPassword
+    ) {
+        const imagePicker = document.querySelector('.overlay') as HTMLDivElement
+        const closeButton = document.querySelector(
+            '.modal-close-button',
+        ) as HTMLButtonElement
+
+        imagePicker.addEventListener('click', showModal)
+        closeButton.addEventListener('click', closeModal)
     }
 })
