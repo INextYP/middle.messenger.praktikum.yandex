68d5eb407d50db7dbcad3169634f2360897d327d
Alexander <oops.krit@yandex.ru>
2024-08-13 11:06:03 +0300

    refactor: profile page

---
+++ src/pages/profile/types.ts
@@ -0,0 +1,12 @@
+import { Input } from '../../components'
+
+export interface ProfilePageProps {}
+
+export interface ProfilePageKeys {
+    form: HTMLFormElement
+    first_name: Input
+    second_name: Input
+    login: Input
+    phone: Input
+    chat_name: Input
+}
