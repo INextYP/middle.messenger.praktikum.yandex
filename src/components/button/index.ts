da10a50470bc1f31257d98769abe0db626d29de4
Alexander <oops.krit@yandex.ru>
2024-08-13 10:47:50 +0300

    refactor: button-component

--- src/components/button/index.ts
+++ src/components/button/index.ts
@@ -1,1 +1,21 @@
-export { default as button } from './button.html?raw'
+import { ButtonProps } from './types'
+
+import buttonTemplate from './button.html?raw'
+import Block from '../../services/block'
+
+export class Button extends Block<ButtonProps> {
+    constructor(props: ButtonProps) {
+        super({
+            ...props,
+            events: {
+                button: {
+                    click: props.onClick,
+                },
+            },
+        })
+    }
+
+    render() {
+        return this.compile(buttonTemplate, this.props)
+    }
+}
