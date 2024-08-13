a0cc2c1c26a9f7d18c95e3f47daf74cb0c9de829
Alexander <oops.krit@yandex.ru>
2024-08-13 11:07:48 +0300

    refactor: chat page

--- src/pages/chat/index.ts
+++ src/pages/chat/index.ts
@@ -1,1 +1,48 @@
-export { default as chatPage } from './chat-page.html?raw'
+import Block, { AttributesType } from '../../services/block'
+import {
+    InputType,
+    ValidationReturnType,
+    validator,
+} from '../../services/valdator'
+import { ChatPageKeys, ChatPageProps } from './types'
+
+import chatPageTemplate from './chat-page.html?raw'
+
+export class ChatPage extends Block<ChatPageProps, ChatPageKeys> {
+    constructor(props: ChatPageProps) {
+        super({
+            ...props,
+            onBlur: (e: Event) => {
+                const { id, value } = e.currentTarget as HTMLInputElement
+
+                const key = id as keyof ChatPageKeys
+                const currentAttribute = this.attributes?.[
+                    key
+                ] as AttributesType
+
+                const [isValid, message] = validator.validate(
+                    id as InputType,
+                    value,
+                ) as ValidationReturnType
+
+                if (!isValid) {
+                    currentAttribute.attributes.errorText.setProps({
+                        errorText: message,
+                    })
+
+                    return false
+                }
+
+                currentAttribute.attributes.errorText.setProps({
+                    errorText: undefined,
+                })
+
+                return true
+            },
+        })
+    }
+
+    render() {
+        return this.compile(chatPageTemplate, this.props)
+    }
+}
