fc48fbfe9200051df73dd318aa7159681f647b51
Alexander <oops.krit@yandex.ru>
2024-08-13 11:06:58 +0300

    refactor: edit-profile page

---
+++ src/pages/edit-profile/types.ts
@@ -0,0 +1,13 @@
+import { Button, Input } from '../../components'
+
+export interface EditProfilePageProps {}
+
+export interface EditProfilePageKeys {
+    form: HTMLFormElement
+    first_name: Input
+    second_name: Input
+    login: Input
+    phone: Input
+    chat_name: Input
+    submitButton: Button
+}
