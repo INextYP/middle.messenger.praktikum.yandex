adbd8485711cc1123a690ba3080df7c5f9544dfa
Alexander <oops.krit@yandex.ru>
2024-08-13 10:57:01 +0300

    refactor: link component

--- src/components/link/index.ts
+++ src/components/link/index.ts
@@ -1,1 +1,14 @@
-export { default as link } from './link.html?raw'
+import Block from '../../services/block'
+import { LinkProps } from './types'
+
+import linkTemplate from './link.html?raw'
+
+export class Link extends Block<LinkProps> {
+    constructor(props: LinkProps) {
+        super(props)
+    }
+
+    render() {
+        return this.compile(linkTemplate, this.props)
+    }
+}
