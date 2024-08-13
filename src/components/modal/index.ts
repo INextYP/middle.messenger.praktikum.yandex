4a223f7845a698358a82bc55a8721e617d263c25
Alexander <oops.krit@yandex.ru>
2024-08-13 10:59:03 +0300

    refactor: modal component

--- src/components/modal/index.ts
+++ src/components/modal/index.ts
@@ -1,1 +1,14 @@
-export { default as modal } from './modal.html?raw'
+import Block from '../../services/block'
+import { ModalProps } from './types'
+
+import modalTemplate from './modal.html?raw'
+
+export class Modal extends Block<ModalProps> {
+    constructor(props: ModalProps) {
+        super(props)
+    }
+
+    render() {
+        return this.compile(modalTemplate, this.props)
+    }
+}
