11772568be6fb79f7636d51dd8605c1ce3f96f9f
Alexander <oops.krit@yandex.ru>
2024-08-13 10:54:42 +0300

    refactor: buildTemplates function

--- src/helpers/build-templates.ts
+++ src/helpers/build-templates.ts
@@ -1,20 +1,12 @@
-import Handlebars from 'handlebars'
-import * as Pages from '../pages'
-import { Routes } from '../app/types'
-import { pageConfig, pageContext } from '../app/constants'
+import * as Components from '../components'
+import { ComponentType, Hbs } from '../services/handlebars'
 
 export const buildTemplates = () => {
-    const pagesArray = Object.entries(Pages)
+    const componentsArray = Object.entries(Components)
+    for (let i = 0; i < componentsArray.length; i++) {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        const [_, Component] = componentsArray[i]
 
-    for (let i = 0; i < pagesArray.length; i++) {
-        const [pageName, pageComponent] = pagesArray[i]
-
-        const key = pageName as `${Routes}`
-
-        const context = pageContext[key]
-
-        const template = Handlebars.compile(pageComponent)(context)
-
-        pageConfig[key] = template
+        Hbs.registerHelper(Component as ComponentType)
     }
 }
