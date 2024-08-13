86d641c1ef567f2227734b07c5a4126d59a8b981
Alexander <oops.krit@yandex.ru>
2024-08-13 11:04:45 +0300

    refactor: 404 and 500 pages

--- src/pages/page-404/index.ts
+++ src/pages/page-404/index.ts
@@ -1,1 +1,14 @@
-export { default as page404 } from './page-404.html?raw'
+import Block from '../../services/block'
+import { Page404Props } from './types'
+
+import page404Template from './page-404.html?raw'
+
+export class Page404 extends Block<Page404Props> {
+    constructor(props: Page404Props) {
+        super(props)
+    }
+
+    render() {
+        return this.compile(page404Template, this.props)
+    }
+}
--- src/pages/page-500/index.ts
+++ src/pages/page-500/index.ts
@@ -1,1 +1,14 @@
-export { default as page500 } from './page-500.html?raw'
+import Block from '../../services/block'
+import { Page500Props } from './types'
+
+import page500Template from './page-500.html?raw'
+
+export class Page500 extends Block<Page500Props> {
+    constructor(props: Page500Props) {
+        super(props)
+    }
+
+    render() {
+        return this.compile(page500Template, this.props)
+    }
+}
