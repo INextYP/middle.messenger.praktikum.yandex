694edbc23c0106eb828acaaccedf8ddffb5d3082
Alexander <oops.krit@yandex.ru>
2024-08-13 11:04:12 +0300

    refactor: message component

--- src/components/message/index.ts
+++ src/components/message/index.ts
@@ -1,1 +1,14 @@
-export { default as message } from './message.html?raw'
+import Block from '../../services/block'
+import { MessageProps } from './types'
+
+import messageTemplate from './message.html?raw'
+
+export class Message extends Block<MessageProps> {
+    constructor(props: MessageProps) {
+        super(props)
+    }
+
+    render() {
+        return this.compile(messageTemplate, this.props)
+    }
+}
