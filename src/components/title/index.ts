2ee2e3a1c38aa4c9f7eaaf351747b2e1519ee680
Alexander <oops.krit@yandex.ru>
2024-08-13 11:07:33 +0300

    refactor: title component

--- src/components/title/index.ts
+++ src/components/title/index.ts
@@ -1,1 +1,14 @@
-export { default as title } from './title.html?raw'
+import { Block } from '../../services/block'
+import { TitleProps } from './types'
+
+import titleTemplate from './title.html?raw'
+
+export class Title extends Block<TitleProps> {
+    constructor(props: TitleProps) {
+        super(props)
+    }
+
+    render() {
+        return this.compile(titleTemplate, this.props)
+    }
+}
