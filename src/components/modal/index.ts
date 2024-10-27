448acc615abb916e5b4859a99fd6f54a66eaf393
Alexander <oops.krit@yandex.ru>
2024-10-27 14:16:33 +0300

    fix: modal ui and partial; delete unused code

--- src/components/modal/index.ts
+++ src/components/modal/index.ts
@@ -1,13 +1,40 @@
 import Block from '../../services/block'
-import { ModalProps } from './types'
-
+import { ModalKeys, ModalProps } from './types'
 import modalTemplate from './modal.html?raw'
 
-export class Modal extends Block<ModalProps> {
+export class Modal extends Block<ModalProps, ModalKeys> {
     static name = 'Modal'
 
     constructor(props: ModalProps) {
-        super(props)
+        super({
+            ...props,
+            events: {
+                form: {
+                    submit: (e: Event) => this.handleSubmit(e),
+                },
+            },
+        })
+    }
+
+    handleSubmit(e: Event) {
+        e.preventDefault()
+        const attributes = this.attributes as ModalKeys
+
+        const newAvatar = attributes.file_input.files?.[0] as File
+
+        attributes.errorText.setProps({
+            errorText: undefined,
+        })
+
+        if (!newAvatar) {
+            attributes.errorText.setProps({
+                errorText: 'Выберите файл',
+            })
+        }
+
+        const callback = this.props.onSubmit as ModalProps['onSubmit']
+
+        callback?.(newAvatar)
     }
 
     render() {
